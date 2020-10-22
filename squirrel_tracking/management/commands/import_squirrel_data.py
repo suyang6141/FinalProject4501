90bd599c6e267183a192210d78009d4309078fcc
Suyang Song <ss6141@columbia.edu>
2020-10-22 09:47:52 +0000

    Import

--- squirrel_tracking/management/commands/import_squirrel_data.py
+++ squirrel_tracking/management/commands/import_squirrel_data.py
@@ -1,61 +1,57 @@
-import os
-import csv 
-import datetime as dt
-from squdata.models import Squirreldata
-from django.core.management.base import BaseCommand, CommandError
-import pytz #pip install pytz
+import csv
+from django.core.management.base import BaseCommand
+from squirrel_tracking.models import Sighting
+from datetime import date
+from dateutil import parser
+import re
 
-# from django.utils.timezone import get_current_timezone
+
 class Command(BaseCommand):
-    help = 'read csv file'
+    help = 'Import the squirrel sightings data...'
     
-    def add_arguments(self, parser):
-        parser.add_argument('squ_file',type=str) 
-    
-    def handle(self, *args, **options):
-        def tobool(string):
-            if string == 'true':
-                return True
-            elif string == 'false':
-                return False 
-
-        with open(options['squ_file']) as csv_file:
-            csv_reader = csv.DictReader(csv_file, delimiter=',')
-            data = list(csv_reader)
-            for item in data:
-                date = dt.datetime.strptime(item['Date'],'%m%d%Y')
-                timezone = pytz.timezone("UTC")
-                date = timezone.localize(date)
-
+    def add_arguments(self,parser):
+        parser.add_argument('file_path', help = 'file containing sighting details')
+        
+    def handle(self, *args, **kwargs):
+        path = kwargs['file_path']
+        pattern = re.compile(r'(\d{2})(\d{2})(\d{4})')
+        
+        with open(path, 'rt') as fp:
+            reader = csv.DictReader(fp)
+            list_ = []
+            
+            
+            for item in reader:
+                month, day, year = pattern.match(item['Date']).groups()
+                obj = Sighting()
+                obj.Longitude = float(item['X'])
+                obj.Latitude = float(item['Y'])
+                obj.Unique_Squirrel_ID = item['Unique Squirrel ID']
+                obj.Shift = item['Shift']
+                obj.Date = date(int(year), int(month), int(day))
+                obj.Age =item['Age']
+                obj.Primary_Fur_Color = item['Primary Fur Color']
+                obj.Location = item['Location']
+                obj.Specific_Location = item['Specific Location']
+                obj.Running = True if item['Running'] == 'TRUE' else False
+                obj.Chasing = True if item['Chasing'] == 'TRUE' else False
+                obj.Climbing = True if item['Climbing'] == 'TRUE' else False
+                obj.Eating = True if item['Eating'] == 'TRUE' else False
+                obj.Foraging = True if item['Foraging'] == 'TRUE' else False
+                obj.Other_Activities = item['Other Activities']
+                obj.Kuks = True if item['Kuks'] == 'TRUE' else False
+                obj.Quaas = True if item['Quaas'] == 'TRUE' else False
+                obj.Moans = True if item['Moans'] == 'TRUE' else False
+                obj.Tail_flags = True if item['Tail flags'] == 'TRUE' else False
+                obj.Tail_twitches =  True if item['Tail twitches'] == 'TRUE' else False
+                obj.Approaches = True if item['Approaches'] == 'TRUE' else False
+                obj.Indifferent = True if item['Indifferent'] == 'TRUE' else False
+                obj.Runs_from = True if item['Runs from'] == 'TRUE' else False
+                list_.append(item['Unique Squirrel ID'])
                 try:
-                    created = Squirreldata(
-                        latitude=item['X'],
-                        longitude=item['Y'],
-                        unique_squirrel_id= item['Unique Squirrel ID'],
-                        shift=item['Shift'],
-                        date=date,
-                        age = item['Age'],
-                        fur_color=item['Primary Fur Color'],
-                        location=item['Location'],
-                        specific_location=item['Specific Location'],
-                        running=tobool(item['Running']),
-                        chasing=tobool(item['Chasing']),
-                        climbing=tobool(item['Climbing']),
-                        eating=tobool(item['Eating']),
-                        foraging=tobool(item['Foraging']),
-                        other_activities=item['Other Activities'],
-                        kuks=tobool(item['Kuks']),
-                        quaas=tobool(item['Quaas']),
-                        moans=tobool(item['Moans']),
-                        tail_flags=tobool(item['Tail flags']),
-                        tail_twitches=tobool(item['Tail twitches']),
-                        approaches=tobool(item['Approaches']),
-                        indifferent=tobool(item['Indifferent']),
-                        runs_from=tobool(item['Runs from']),
-                        )
-                    created.save()
-           
-                except FileNotFoundError:
-                    raise CommandError("File {} does not exist".format(file_path))
-
-
+                    obj.save()
+                    msg = f'You are importing data from {path}'
+                    self.stdout.write(self.style.SUCCESS(msg))
+                except Exception as e:
+                    ms = f'Error: {e} at {obj.Unique_Squirrel_ID}'
+                    self.stdout.write(self.style.ERROR(ms))
