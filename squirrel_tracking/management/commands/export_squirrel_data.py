900b0ad157e91b0afb9abdf15a501afaa5c76497
Suyang Song <ss6141@columbia.edu>
2020-10-22 14:16:25 +0000

    Export

--- squirrel_tracking/management/commands/export_squirrel_data.py
+++ squirrel_tracking/management/commands/export_squirrel_data.py
@@ -3,4 +3,41 @@ import datetime, csv
 from squdata.models import Squirreldata
 from django.apps import apps
 
-
+class Command(BaseCommand):
+    help = 'Export all data to a csv file'
+    def add_arguments(self, parser):
+        parser.add_argument('path', type=str)
+    def handle(self, *args, **kwargs):
+        path = kwargs['path']
+        with open(path, 'w', newline='') as csvfile:
+            attributes = [
+                    'Latitude',
+                    'Longitude',
+                    'Unique_Squirrel_ID',
+                    'Shift',
+                    'Date',
+                    'Age',
+                    'Primary_Fur_Color',
+                    'Location',
+                    'Specific_Location',
+                    'Running',
+                    'Chasing',
+                    'Climbing',
+                    'Eating',
+                    'Foraging',
+                    'Other_Activities',
+                    'Kuks',
+                    'Quaas',
+                    'Moans',
+                    'Tail_flags',
+                    'Tail_twitches',
+                    'Approaches',
+                    'Indifferent',
+                    'Runs_from'
+            ]
+            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            writer.writerow(attributes)
+            for row in Sighting.objects.all():
+                writer.writerow([getattr(row, attribute) for attribute in attributes])
+        msg=f'Exporting sighting data to {path}...Done!'
+        self.stdout.write(self.style.SUCCESS(msg))
